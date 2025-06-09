2b45a652da5f8c2fee2454c58dfd84de85622b66
Giga Mania <hil.giga@gmail.com>
2025-06-09 22:40:04 +0100

    fix: correct path reference for logo; adjust sizing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="border-t py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
         <div className="flex items-center gap-2">
-          <Image src="/guspump.png" alt="logo" width={40} height={40}/>
+          <Image src="/logo.png" alt="logo" width={20} height={30}/>
           <p className="text-sm leading-loose text-muted-foreground md:text-base">
             Fuellytics &copy; {new Date().getFullYear()}
           </p>
